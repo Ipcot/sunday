d8fae04b7b6b7ddba3d76c635d169a98054623d3
Yuriy Pokhyl <woully@gmail.com>
2022-07-24 17:33:43 +0300

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,10 @@
-import { Section } from './Section/Section';
-import { FeaturesList } from './FeaturesList/FeaturesList';
-import { TeamList } from './TeamList/TeamList';
+import { GlobalStyle } from './GlobalStyle';
 
-import featuresData from '../data/features.json';
-import teammembers from '../data/teammembers.json';
 export const App = () => {
   return (
     <>
-      <Section>
-        <FeaturesList data={featuresData} />
-      </Section>
-      <Section title="Наша команда" teammembers={teammembers}>
-        <TeamList teammembers={teammembers}></TeamList>
-      </Section>
+      <div>Test</div>
+      <GlobalStyle />
     </>
   );
 };
