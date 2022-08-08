0d7aafcd2afbfe5ee44460c576aa79db23a2b36c
Yuriy Pokhyl <woully@gmail.com>
2022-08-08 23:06:33 +0300

    styled

---
+++ src/components/AddForm/AddForm.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  /* flex-direction: column; */
+  justify-content: center;
+`;
+
+export const Label = styled.label`
+  margin: 0 auto;
+  padding-left: 30px;
+`;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import { Home } from './Home/Home';
-import { AddForm } from './AddForm/AddForm';
+import Home from './Home/Home';
+import AddForm from './AddForm/AddForm';
 
 export const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/add" element={<AddForm />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/add" element={<AddForm />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 };
